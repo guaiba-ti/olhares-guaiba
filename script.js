06059e0ee10e854aeb1505140a8e6c9b1dd49ba3
Rafael Andriotti Rebelo <rafael.rebelo@GUAIBA.LOCAL>
2026-07-10 15:20:28 -0300

    Adicionando link formularios 2026

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function openForm(category) {
     if (category === 'estudantil') {
         // Abrir formulário do Google para estudantes
-        window.open('https://forms.gle/SKrXBGMHdzPPGxbV9', '_blank');
+        window.open('https://forms.gle/gL1h4oZA3wdYv8p7A', '_blank');
     } else if (category === 'comunidade') {
         // Abrir formulário do Google para comunidade
-        window.open('https://forms.gle/dHrk63hT52ewzybp7', '_blank');
+        window.open('https://forms.gle/fYwAuj1dBC64JwiN8', '_blank');
     }
 }
